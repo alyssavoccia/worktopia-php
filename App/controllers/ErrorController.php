edfb1be5f469ac025cad457eae649f97e9d68a74
Alyssa Voccia <avoccia@hotmail.com>
2024-03-28 06:28:48 -0400

    Added params to our router

--- App/controllers/ErrorController.php
+++ App/controllers/ErrorController.php
@@ -11,6 +11,7 @@ class ErrorController {
    */
   public static function notFound($message = 'Resource not found') {
     http_response_code(404);
+
     loadView('error', [
       'status' => '404',
       'message' => $message
@@ -24,6 +25,7 @@ class ErrorController {
    */
   public static function unauthorized($message = 'You are not authorized to view this resource') {
     http_response_code(403);
+    
     loadView('error', [
       'status' => '403',
       'message' => $message
