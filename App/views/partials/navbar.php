6310f5ce05234e5774985a254d289a8f99736b04
Alyssa Voccia <avoccia@hotmail.com>
2024-03-31 16:56:38 -0400

    Added the functionality to logout and clear the session

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -11,7 +11,7 @@
       <?php if (Session::has('user')) : ?>
         <div class="flex justify-between items-center gap-4">
           <div class="">Welcome, <?= Session::get('user')['name'] ?></div>
-          <form>
+          <form method="POST" action="/auth/logout">
             <button type="submit" class="text-white inline hover:underline">Logout</button>
           </form>
           <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
