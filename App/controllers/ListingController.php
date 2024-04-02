d786299b82f881a3483bc7a322a069d59d671842
Alyssa Voccia <avoccia@hotmail.com>
2024-04-02 05:33:37 -0400

    Updated authorization so only owners of listings can edit/see edit and delete buttons on their listings

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -182,6 +182,12 @@ class ListingController {
       return;
     }
 
+    // Authorization
+    if (!Authorization::isOwner($listing->user_id)) {
+      Session::setFlashMessage('error_message','You are not authorized to edit this listing');
+      return redirect('/listings/' . $listing->id);
+    }
+
     loadView("listings/edit", [
       "listing" => $listing
     ]);
@@ -206,6 +212,12 @@ class ListingController {
     if (!$listing) {
       ErrorController::notFound('Listing not found');
       return;
+    }
+
+    // Authorization
+    if (!Authorization::isOwner($listing->user_id)) {
+      Session::setFlashMessage('error_message','You are not authorized to update this listing');
+      return redirect('/listings/' . $listing->id);
     }
 
     $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
